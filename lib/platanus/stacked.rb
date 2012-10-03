a74a6bf62dbf431661b86ad40c8ed0a94a826fef
Ignacio Baixas <iobaixas@gmai.com>
2012-10-03 16:52:27 -0300

    stacked: removes autosave support, adds top_id support, improves consistency and cached prop managing

--- lib/platanus/stacked.rb
+++ lib/platanus/stacked.rb
@@ -8,6 +8,8 @@ module Platanus
   #
   module StackedAttr
 
+    class NotSupportedError < StandardError; end
+
     def self.included(base)
       base.extend ClassMethods
     end
@@ -18,70 +20,77 @@ module Platanus
       def has_stacked(_name, _options={}, &block)
 
         tname = _name.to_s
+        tname_single = tname.singularize
+
         to_cache = _options.delete(:cached)
         to_cache_prf = if _options[:cache_prf].nil? then 'last_' else _options.delete(:cache_prf) end
-        stack_key = if _options[:stack_key].nil? then 'created_at DESC, id DESC' else _options.delete(:stack_key) end
 
         # Retrieve callbacks
         before_push = _options.delete(:before_push)
         after_push = _options.delete(:after_push)
 
-        _options[:order] = stack_key
+        raise NotSupportedError.new('Only autosave mode is supported') if _options[:autosave] == false
+
+        _options[:order] = 'created_at DESC, id DESC'
         _options[:limit] = 10 if _options[:limit].nil?
 
-        # Protect cached attributes.
+        # Prepare cached attributes, generate read-only aliases without prefix.
         unless to_cache.nil?
-          to_cache = to_cache.map { |name| name.to_s }
-          to_cache.each { |name| attr_protected(to_cache_prf + name) }
-        end
-
-        private
-        has_many(_name,_options)
-        public
-
-        send :define_method, 'push_' + tname[0...-1] + '!' do |obj|
-          self.class.transaction do
-
-            # Execute before callbacks
-            self.send(before_push, obj) unless before_push.nil?
-            block.call(self, obj) unless block.nil?
-
-            # Cache required fields
-            unless to_cache.nil?
-              to_cache.each { |name| send(to_cache_prf + name + '=', obj.send(name)) }
-            end
-
-            # Save model and push attribute, cache last stacked attribute.
-            self.save! if _options.fetch(:autosave, true) and (self.new_record? or self.changed?)
-            raise ActiveRecord::RecordInvalid.new(obj) unless self.send(tname).send('<<',obj)
-            @_stacked_last = obj
-
-            # Execute after callback
-            self.send(after_push, obj) unless after_push.nil?
+          to_cache = to_cache.map do |name|
+            name = name.to_s; fullname = to_cache_prf + name
+            attr_protected(fullname)
+            send :define_method, name { self.send(fullname) }
+            fullname
           end
         end
 
-        send :define_method, 'push_' + tname[0...-1] do |obj|
+        private
+        has_many _name, _options
+        public
+
+        send :define_method, "push_#{tname_single}!" do |obj|
+          self.class.transaction do
+
+            # execute before callbacks
+            self.send(before_push, obj) unless before_push.nil?
+            block.call(self, obj) unless block.nil?
+
+            # cache required fields
+            unless to_cache.nil?
+              to_cache.each { |name| send(name + '=', obj.send(name)) }
+            end
+
+            # push attribute, this will save the model if new.
+            raise ActiveRecord::RecordInvalid.new(obj) unless self.send(tname).send('<<',obj)
+
+            # update inverse association if posible
+            if self.attributes.has_key? "top_#{tname_single}_id"
+              self["top_#{tname_single}_id"] = obj.id
+            else
+              @_stacked_last = obj
+            end
+
+            # execute after callback
+            self.send(after_push, obj) unless after_push.nil?
+
+            self.save! if self.changed? # Must save again, no other way...
+          end
+        end
+
+        send :define_method, "push_#{tname_single}" do |obj|
           begin
-            return self.send('push_' + tname[0...-1] + '!', obj)
+            return self.send("push_#{tname_single}!", obj)
           rescue ActiveRecord::RecordInvalid
             return false
           end
         end
 
-        send :define_method, 'top_' + tname[0...-1] do
-          # Storing the last stacked value will not prevent race conditions
-          # when simultaneous updates occur.
-          return @_stacked_last unless @_stacked_last.nil?
-          @_stacked_last = self.send(_name).first
-        end
-
-        # Generate shorcut properties for cached attributes.
-        unless to_cache.nil?
-          to_cache.each do |name|
-            send :define_method, name.to_s do
-              self.send(to_cache_prf + name)
-            end
+        unless self.column_names.include? "top_#{tname_single}_id"
+          send :define_method, "top_#{tname_single}" do
+            # Storing the last stacked value will not prevent race conditions
+            # when simultaneous updates occur.
+            return @_stacked_last unless @_stacked_last.nil?
+            @_stacked_last = self.send(_name).first
           end
         end
       end
