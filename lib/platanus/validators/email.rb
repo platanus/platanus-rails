5c0d00df4829dbd8ab6011cd114bcfe079fef588
Ignacio Baixas <iobaixas@gmai.com>
2012-10-10 11:28:56 -0300

    adds email validator

---
+++ lib/platanus/validators/email.rb
@@ -0,0 +1,23 @@
+require 'mail'
+
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(_record, _attribute, _value)
+    return if _value.nil?
+    begin
+      mail = Mail::Address.new(_value)
+      # We must check that value contains a domain and that value is an email address
+      res = mail.domain && mail.address == _value
+      tree = mail.__send__(:tree)
+      # We need to dig into treetop
+      # A valid domain must have dot_atom_text elements size > 1
+      # user@localhost is excluded
+      # treetop must respond to domain
+      # We exclude valid email values like <user@localhost.com>
+      # Hence we use mail.__send__(tree).domain
+      res &&= (tree.domain.dot_atom_text.elements.size > 1)
+    rescue Exception => e
+      res = false
+    end
+    _record.errors[_attribute] << (options[:message] || "is invalid") unless res
+  end
+end
