9d2bc58c61040ae06a9ef698470b418ef0a1317b
Ignacio Baixas <iobaixas@gmai.com>
2012-12-19 00:17:51 -0300

    stacked2: adds callback helpers

--- lib/platanus/stacked2.rb
+++ lib/platanus/stacked2.rb
@@ -52,10 +52,23 @@ module Platanus
             # Storing the last stacked value will not prevent race conditions
             # when simultaneous updates occur.
             last = instance_variable_get top_value_var
-            return last unless last.nil?
-            instance_variable_set(top_value_var, self.send(tname).first)
+            return last unless last.nil? or !last.persisted?
+            instance_variable_set(top_value_var, self.send(tname).all.first)
           end
           nil
+        end
+
+        # When called inside callbacks, returns the new value being put at top of the stack.
+        new_value_var = "@_stacked_#{tname}_new"
+        send :define_method, "#{tname_single}_will" do
+          instance_variable_get(new_value_var)
+        end
+
+        # When called inside callbacks, will return the top value unless a new value is
+        # being pushed, in that case it returns the new value
+        last_value_var = "@_stacked_#{tname}_last"
+        send :define_method, "#{tname_single}_is" do
+          instance_variable_get(last_value_var)
         end
 
         # Prepare cached attributes
@@ -77,7 +90,63 @@ module Platanus
           end
         end
 
-        # Generate mirroring attributes (if mirroring is active)
+        # register callbacks
+        define_callbacks "stack_#{tname_single}"
+
+        # push logic
+        __update_stack = ->(_ctx, _top, _new_top, _save_quiet, &_block) do
+          begin
+            # make xx_top_value avaliable for event handlers
+            _ctx.instance_variable_set(new_value_var, _top) if _new_top
+            _ctx.instance_variable_set(last_value_var, _top)
+
+            _ctx.run_callbacks "stack_#{tname_single}" do
+
+              # cache required fields
+              # TODO: improve cache: convention over configuration!
+              # cache should be automatic given certain column names and should include aliased attribues and virtual attributes.
+              # has_stacked :things, cache: { prefix: '', aliases: { xx => xx }, exclude: [], virtual: { xx => xx } }
+              if to_cache
+                to_cache.each do |cache_attr|
+                  value = if cache_attr.has_key? :from
+                    _top.nil? ? nil : _top.send(cache_attr[:from])
+                  else
+                    _ctx.send(cache_attr[:virtual])
+                  end
+                  _ctx.send(cache_attr[:to].to_s + '=', value)
+                end
+              end
+
+              _block.call if _block
+
+              if _new_top
+                # TODO: this leaves the invalid record on top of the stack and invalid cached values,
+                # maybe validation should ocurr before caching...
+                raise ActiveRecord::RecordInvalid.new(_top) unless _ctx.send(tname) << _top
+              end
+
+              # reset top_value_prop to top
+              if top_value_key
+                if _save_quiet
+                  top_id = if _top.nil? then nil else _top.id end
+                  if _ctx.send(top_value_key) != top_id
+                    _ctx.update_column(top_value_key, top_id)
+                    _ctx.send(top_value_prop, false) # reset belongs_to cache
+                  end
+                else
+                  _ctx.send("#{top_value_prop}=", _top)
+                end
+              else
+                _ctx.instance_variable_set(top_value_var, _top)
+              end
+            end
+          ensure
+            _ctx.instance_variable_set(new_value_var, nil)
+            _ctx.instance_variable_set(last_value_var, nil)
+          end
+        end
+
+        # Attribute mirroring
         #
         # Mirroring allows using the top value attributes in the parent model,
         # it also allows modifying the attributes in the parent model, if the model is
@@ -109,84 +178,48 @@ module Platanus
                 return top.send attr_name
               end
 
+              send :define_method, "#{attr_name}_changed?" do
+                mirror = instance_variable_get(mirror_cache_var)
+                return true if !mirror.nil? and mirror.has_key? attr_name
+                return self.send(prefix + attr_name + '_changed?') if self.respond_to? prefix + attr_name + '_changed?' # return cached value if avaliable
+                return true # for now just return true for non cached attributes
+              end
+
               attr_accessible attr_name
             else
               Rails.logger.warn "stacked: failed to mirror getter for #{attr_name} in #{self.to_s}"
             end
           end
-        end
 
-        # setup main association
-        # TODO: Support other kind of ordering, this would require to reevaluate top on every push
-        _options[:order] = 'created_at DESC, id DESC'
-        _options[:limit] = 10 if _options[:limit].nil?
-        has_many _name, _options
+          # before saving model, load changes from virtual attributes.
+          set_callback :save, :around do |&_block|
 
-        # register callbacks
-        define_callbacks "stack_#{tname_single}"
+            mirror = instance_variable_get(mirror_cache_var)
+            if !mirror.nil? and mirror.count > 0
 
-        # push logic
-        __push = ->(_ctx, _top, _top_is_new, _save_top, _proc) do
-          _ctx.run_callbacks "stack_#{tname_single}", _top, _top_is_new do
-
-            # cache required fields
-            if to_cache
-              to_cache.each do |cache_attr|
-                value = if cache_attr.has_key? :from
-                  _top.nil? ? _top : _top.send(cache_attr[:from])
-                else
-                  _ctx.send(cache_attr[:virtual], _top, _top_is_new)
+              # propagate non cached attributes (only if record is not new and there is a top state)
+              unless self.new_record?
+                top = self.send top_value_prop
+                unless top.nil?
+                  stacked_model.accessible_attributes.each do |attr_name|
+                    mirror[attr_name] = top.send(attr_name) unless mirror.has_key? attr_name
+                  end
                 end
-                _ctx.send(cache_attr[:to].to_s + '=', value)
               end
-            end
 
-            _proc.call if _proc
+              obj = stacked_model.new(mirror)
+              instance_variable_set(mirror_cache_var, {}) # reset mirror changes
+              __update_stack.call(self, obj, true, true, &_block)
 
-            if _top_is_new
-              # Save if new
-              raise ActiveRecord::RecordInvalid.new(_top) unless send(tname).send('<<', _top)
-
-              if top_value_key
-                if _save_top
-                  self.update_column(top_value_key, _top.id)
-                  _ctx.send(top_value_prop, false) # reset belongs_to cache
-                else
-                  _ctx.send("#top_value_prop}=", _top)
-                end
-              else
-                instance_variable_set(top_value_var, _top)
-              end
-            end
+            else _block.call end
           end
         end
 
-        # before saving model, load changes from virtual attributes.
-        set_callback :save, :around do
-
-          mirror = instance_variable_get(mirror_cache_var)
-          if !mirror.nil? and mirror.count > 0
-
-            # propagate non cached attributes (only if record is not new and there is a top state)
-            unless self.new_record?
-              top = self.send top_value_prop
-              unless top.nil?
-                stacked_model.accessible_attributes.each do |attr_name|
-                  mirror[attr_name] = top.send(attr_name) unless mirror.has_key? attr_name
-                end
-              end
-            end
-
-            obj = stacked_model.new(mirror)
-            instance_variable_set(mirror_cache_var, {}) # reset mirror changes
-            __push.call(self, obj, true, true, Proc.new { yield })
-
-          else yield end
-        end
+        # Push methods
 
         send :define_method, "push_#{tname_single}!" do |obj|
           self.class.transaction do
-            __push.call(self, _top, true, false, Proc.new { self.save! if self.new_record? })
+            __update_stack.call(self, obj, true, false) { self.save! if self.new_record? }
             self.save! if self.changed?
           end
         end
@@ -199,10 +232,12 @@ module Platanus
           end
         end
 
+        # Restore methods
+
         send :define_method, "restore_#{tname}!" do
           self.class.transaction do
-            # find current top, then restore stack state
-            __push.call(self, self.send(_name).first, false, false, nil)
+            top = self.send(tname).all.first
+            __update_stack.call(self, top, false, false)
             self.save! if self.changed?
           end
         end
@@ -214,6 +249,13 @@ module Platanus
             return false
           end
         end
+
+        # setup main association
+        # TODO: Support other kind of ordering, this would require to reevaluate top on every push
+        _options[:order] = 'created_at DESC, id DESC'
+        _options[:limit] = 1 if _options[:limit].nil?
+        _options.delete(:limit) if _options[:limit] == :no_limit
+        has_many _name, _options
       end
     end
   end
--- lib/platanus/version.rb
+++ lib/platanus/version.rb
@@ -1,3 +1,3 @@
 module Platanus
-  VERSION = "0.0.36" # 0.1 will come with tests!
+  VERSION = "0.0.48" # 0.1 will come with tests!
 end
