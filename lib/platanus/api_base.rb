f127afff0117748f2add9808bcbc511fd9f170e4
Ignacio Baixas <iobaixas@gmai.com>
2013-02-21 12:49:14 -0300

    apibase: simplyfies response logics

--- lib/platanus/api_base.rb
+++ lib/platanus/api_base.rb
@@ -52,23 +52,6 @@ module Platanus
 
   module ClassMethods
 
-    # # Enables dumb object wrapping
-    #
-    # When enabled, every request parameter is wrapped in a user defined key.
-    # You should disable default parameter wrapping when using this.
-    # (look at wrap_parameter.rb file for more information on how to disable parameter wrapping)
-    #
-    # @param [Symbol] _key Params key to store parameters in (defaults to :object)
-    #
-    def dumb_wrapping(_key=:object)
-      self.before_filter do
-        # Disable magic parameter wrapping (at env) and use 'object' key to hold
-        # incomming request parameters.
-        return if request.headers['HTTP_USER_AGENT'] == 'Rails Testing'
-        params[:object] = request.request_parameters
-      end
-    end
-
     # # Enables cors
     def enable_cors
       # TODO: maybe cors support should be implemented as a middleware.
@@ -81,27 +64,22 @@ module Platanus
 
   module InstanceMethods
 
-    # # Renders an empty response
+    ## Renders an empty response
+    #
     def api_respond_empty(_options={})
       _options[:json] = {}
       api_respond_with(_options)
     end
 
-    # # Renders a regular response
-    def api_respond_with(*_params)
-      if _params.last.is_a? Hash
-        _options = _params.last
-        _params = _params[0...-1]
-      else
-        _options = {}
-      end
-
-      respond_with(*_params) do |format|
+    ## Renders a regular response
+    #
+    def api_respond_with(_resource, _options={})
+      respond_with(_resource) do |format|
         format.json { render _options }
       end
     end
 
-    # # Renders an error response
+    ## Renders an error response
     #
     # @param [String, Fixnum] _status Response error code.
     # @param [object] _error_obj Error object to serialize in response.
